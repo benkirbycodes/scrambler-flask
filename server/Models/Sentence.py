920e19a7c4d33e130f3e85e1230684ffa391d690
benkirbycodes <benkirbycodes@gmail.com>
2020-03-05 13:25:58 -0700

    Sentence Model

--- server/Models/Sentence.py
+++ server/Models/Sentence.py
@@ -2,5 +2,6 @@ from sqlalchemy import Column
 from sqlalchemy.types import Integer, Text, String
 
 
-class User(db.Model):
-    id = Column(Integer, primary_key=True, nullable=False)
+class Sentence(db.Model):
+    id = Column(Integer, primary_key=True, nullable=False, auto_increment=True)
+    text = Column(String(120), nullable=False)
